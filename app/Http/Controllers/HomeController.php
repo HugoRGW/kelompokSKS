3276946e54c417eb473f622d52fb56ec4a362da3
lontongbalap <90811663+Mayfiq@users.noreply.github.com>
2023-10-10 08:26:22 +0700

    personalinformation admin sudah selesai

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Info;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -28,6 +29,8 @@ class HomeController extends Controller
 
     public function contact()
     {
-        return view('User/contact');
+        $info = Info::all();
+
+        return view('User/contact', compact('info'));
     }
 }
